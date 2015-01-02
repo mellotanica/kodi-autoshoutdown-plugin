6287faaeecbf022b4b2bc4dfab5fab70ab9ed1a5
Marco Melletti <mellotanica@hotmail.it>
2015-01-02 02:18:24 +0100

    second failure

--- addon.py
+++ addon.py
@@ -4,18 +4,40 @@ import xbmc
 #xbmc.executebuiltin("ShutDown")
 
 class MyPlayer(xbmc.Player):
-    def onPlayBackEnded(self):
-        xbmcgui.Dialog().ok("play ended")
+    def __init__(self, *args, **kwargs):
+        xbmc.Player.__init__(self)
+        self.myplaycount = 0
+        self.mylastplay = 0
+        self.myshutdownenabled = False
+        self.mylockstate = False
     
+    def onPlayBackEnded(self):
+        self.onPlayBackStopped()
+    
+    def onPlayBackStopped(self):
+        print "playback stopped"
+        myUnlock()
+    
+    def myLock(self):
+        self.mylockstate = True
+    
+    def myUnlock(self):
+        self.mylockstate = False
+    
+    def getLockState(self):
+        return self.mylockstate
 
+        
 count = 0
 
 heading = "How many items will you watch?"
 
 number = xbmcgui.Dialog().numeric(0,heading)
 
-debug1 = "inserted number: "+number+", count: "+str(count)+", is playing: "+str(xbmc.Player().isPlaying())
+Player = MyPlayer()
+
+Player.myLock()
+
+debug1 = "inserted number: "+number+", count: "+str(count)+", is playing: "+str(Player.isPlaying())+", lock state:"+str(Player.getLockState())
 
 xbmcgui.Dialog().ok("DEBUG", debug1)
-
-xbmc.Player = MyPlayer()
